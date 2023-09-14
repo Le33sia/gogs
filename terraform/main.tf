ad18de16724f9d28bd5bb13eac97cb45fa605ce0
Le33sia <117647628+Le33sia@users.noreply.github.com>
2023-09-14 09:27:17 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,8 +13,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
-  access_key = "AKIA6QNZUG762ZSROCCP"
-  secret_key = "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"
 }
 resource "aws_vpc" "myvpc"{
     cidr_block = "10.0.0.0/16"
